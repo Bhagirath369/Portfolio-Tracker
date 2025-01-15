b389cc0670300efa057910ae2222e4de1dc48602
hi-8055 <pantadevendra16@gmail.com>
2025-01-15 14:18:42 +0545

    adding portfolio

--- TrackFolio/TrackFolio/views.py
+++ TrackFolio/TrackFolio/views.py
@@ -132,12 +132,74 @@ def login(request):
 
 
 def home(request):
-    if 'user_id' not in request.session:
-        # If the user is not logged in, redirect to login page
-        return redirect('login')
+    # if 'user_id' not in request.session:
+    #      # If the user is not logged in, redirect to login page
+    #      return redirect('login')
 
     # Otherwise, render the home page
-    return render(request, 'home.html')
+    #return render(request, 'home.html')
+
+    user_id = request.session.get('user_id')  # Assuming user ID is stored in session
+    if not user_id:
+        return redirect('login')
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            SELECT portfolio_id, name
+            FROM portfolio
+            WHERE user_id = %s
+        """, [user_id])
+        portfolios = cursor.fetchall()  # Returns a list of tuples (portfolio_id, name)
+
+    return render(request, 'home.html', {'portfolios': portfolios})
+
+
+def add_portfolio(request):
+    user_id = request.session.get('user_id')  # Assuming user ID is stored in session
+    if not user_id:
+        return redirect('login')
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        portfolio_type = request.POST.get('type')
+
+        # Ensure both name and portfolio_type are provided
+        if not name or not portfolio_type:
+            return render(request, 'add_portfolio.html', {
+                'error': 'Both name and type are required!'
+            })
+
+
+        with connection.cursor() as cursor:
+            cursor.execute("""
+                INSERT INTO portfolio (user_id, name, total_investment, current_value,
+                                       sold_value, realised_gain, estimated_loss, today_gain_or_loss, type)
+                VALUES (%s, %s, 0, 0, 0, 0, 0, 0, %s)
+            """, [user_id, name, portfolio_type])
+
+        return redirect('home')  # Redirect to the dashboard after successful creation
+
+    return render(request, 'add_portfolio.html')
+
+
+def portfolio(request, portfolio_id):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT portfolio_id, name, type FROM portfolio WHERE portfolio_id = %s", [portfolio_id])
+        portfolio = cursor.fetchone()  # Returns a single tuple (id, name, type) or None
+
+    if not portfolio:
+        return redirect('home')  # Redirect if portfolio does not exist
+
+    return render(request, 'portfolio.html', {'portfolio': portfolio})
+    
+
+def delete_portfolio(request, portfolio_id):
+    with connection.cursor() as cursor:
+        cursor.execute("DELETE FROM portfolio WHERE portfolio_id = %s", [portfolio_id])
+
+    return redirect('home')  # Redirect to the list view
+
+
+
 
 
 
