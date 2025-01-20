42048818478044451fd06cd86ed04775bf626430
Bhagirath369 <bhagiratharyal2@gmail.com>
2025-01-20 21:03:28 +0545

    returned stocks name to add_transaction

--- TrackFolio/TrackFolio/livedata.py
+++ TrackFolio/TrackFolio/livedata.py
@@ -1,16 +1,27 @@
+
+
+
 from bs4 import BeautifulSoup
 import requests
-import lxml
 
-# site for live data
-url = "https://merolagani.com/LatestMarket.aspx#0"
+def fetch_live_stock_data():
+    url = "https://merolagani.com/LatestMarket.aspx#0" # getting data from merolagani
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, 'lxml')
 
-req = requests.get(url)  # making requests to  fetch the raw html data
-soup = BeautifulSoup(req.text,'lxml')
-table = soup.find('table', class_ ="table table-hover live-trading sortable")
-headers = [i.text for i in table.find_all('th')] # data of the tag 'th'
-
-data =[ j for j in table.find_all('tr',{"class":['decrease-row','increase-row', 'nochange-row']})]
-
-result = [{headers[index]:cell.text for index,cell in enumerate(row.find_all('td'))} for row in data]
-print(result)
+    # Find the table that contains the stock data
+    table = soup.find('table', class_="table table-hover live-trading sortable")
+    
+    # Extract headers and data
+    headers = [i.text.strip() for i in table.find_all('th')]
+    data = [j for j in table.find_all('tr', {"class": ['decrease-row', 'increase-row', 'nochange-row']})]
+    
+    # Create a list of stock symbols and names
+    stocks = [
+    {
+        'stock_symbol': row.find_all('td')[0].find('a').text.strip(),  # Get stock symbol from <a> tag
+        'stock_name': row.find_all('td')[0].find('a')['title'].strip()  # Get stock name from 'title' attribute of <a> tag
+    }
+    for row in data
+    ]
+    return stocks
